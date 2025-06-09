2a236fac07fb26e1fae6411e5e67cfc5275aade3
Ellys Gorodisch <125921991+TheDarkThatBarks@users.noreply.github.com>
2025-06-09 14:12:24 -0400

    Squashed commit of the following:
    commit dfffd8fd8419e9af82f1a893078f92a426a2a02b
    Author: Ellys Gorodisch <125921991+TheDarkThatBarks@users.noreply.github.com>
    Date:   Mon Jun 9 14:12:15 2025 -0400
    
        Added landscape to portrait transition
    
    commit f77f88eb0ed51f18bdcd711e1313911d05be47c8
    Author: Ellys Gorodisch <125921991+TheDarkThatBarks@users.noreply.github.com>
    Date:   Sat Jun 7 16:21:02 2025 -0400
    
        Mobile almost done
    
    commit 9633ebe66f8f4556bacd81b9b8724d0ad73bc724
    Author: Ellys Gorodisch <125921991+TheDarkThatBarks@users.noreply.github.com>
    Date:   Fri Jun 6 20:57:58 2025 -0400
    
        Got rid of hidden-nav
    
    commit 38b058cbe9468796182742504c7668942bdd189b
    Author: Ellys Gorodisch <125921991+TheDarkThatBarks@users.noreply.github.com>
    Date:   Fri Jun 6 17:48:22 2025 -0400
    
        A lot
    
    commit 5c1c57673b82ce36d4eb8496fcdf336b9533d5be
    Author: Ellys Gorodisch <125921991+TheDarkThatBarks@users.noreply.github.com>
    Date:   Sat May 31 11:30:09 2025 -0400
    
        Big changes to project opening/closing

--- index.js
+++ index.js
@@ -1,97 +1,168 @@
-let body, projectWidth, projectHeight;
-const aspectRatio = 20/9;
-const widthPercent = 0.8;
-const heightPercent = 0.9;
-
+let widthPercent, heightPercent;
 let currentProject = null;
-let projectClosing = false;
 
-const desktop = window.matchMedia("(orientation: landscape)").matches;
-
-const refresh = function () {
-    body = document.body.getBoundingClientRect();
-    projectWidth = widthPercent * window.visualViewport.width;
-    projectHeight = heightPercent * window.visualViewport.height;
-    const button = currentProject.querySelector(".close");
-    button.style.fontSize = `${projectHeight * 0.08 * 0.8}px`;
-    const details = currentProject.querySelector(".details-table")
-    if (details)
-        details.style.gridTemplateRows = `repeat(${desktop ? 3 : 2}, ${desktop ? (projectHeight * 0.35 / 3) : (projectHeight * 0.24 / 2)}px)`;
-    //console.log(getComputedStyle(currentProject.querySelector(".details-table")).gridTemplateRows);
+window.onload = function (event) {
+    backgroundAnimation();
 };
 
-const delay = (time) => new Promise(resolve => setTimeout(resolve, time));
+const mql = window.matchMedia("(orientation: landscape)");
+let landscape = mql.matches;
+mql.onchange = function (event) {
+    if (event.matches !== landscape && currentProject) {
+        landscape = event.matches;
+        refresh();
+        currentProject.style.display = "none";
+        currentProject = document.querySelector(`.${landscape ? "landscape" : "portrait"} > #${currentProject.id}`);
+        const newTop = landscape ? `${(100 - heightPercent * 100) / 2}vh` : "2.5px";
+        const newLeft = landscape ? `${(100 - widthPercent * 100) / 2}vw` : "2.5px";
+        const newWidth = `${widthPercent * 100}vw`;
+        const newHeight = `${heightPercent * 100}vh`;
+        currentProject.style.top = newTop;
+        currentProject.style.left = newLeft;
+        currentProject.style.width = newWidth;
+        currentProject.style.height = newHeight;
+        currentProject.style.opacity = "1";
+        currentProject.style.fontSize = "16px";
+        currentProject.style.display = "flex";
+        const clone = document.querySelector("#clone");
+        clone.style.top = newTop;
+        clone.style.left = newLeft;
+        clone.style.width = newWidth;
+        clone.style.height = newHeight;
+    } else {
+        landscape = event.matches;
+    }
+    fixAnchors();
+};
 
-const showProject = function(thumbnail) {
+const fixAnchors = function () {
+    const nav = document.querySelector("nav");
+    document.querySelectorAll(".anchor").forEach((element) => {
+        element.style.scrollMarginTop = landscape ? `${nav.getBoundingClientRect().height + 30}px` : "0";
+    });
+};
+
+const refresh = function () {
+    widthPercent = landscape ? 0.95 : 0.99;
+    heightPercent = landscape ? 0.95 : 0.99;
+    const projectHeight = heightPercent * window.visualViewport.height;
+
+    const lastCol = currentProject.querySelector(".column-small:nth-last-child(2)");
+    if (lastCol) {
+        lastCol.style.marginRight = `-${projectHeight * 0.08 + 26}px`;
+
+        const lastColFirstItem = currentProject.querySelector(".column-small:nth-last-child(2) > *:first-child");
+        if (lastColFirstItem)
+            lastColFirstItem.style.marginTop = `${projectHeight * 0.1}px`;
+    }
+    
+    const details = currentProject.querySelector(".details-table");
+    if (details)
+        details.style.gridTemplateRows = `repeat(3, auto)`;
+};
+
+const showProject = function (thumbnail) {
     if (currentProject) {
-        //console.log("DEBUG");
         closeProject();
         return;
     }
-    const project = document.querySelector(`.${desktop ? "desktop" : "mobile"} > #${thumbnail.classList[0]}`);
-    currentProject = project;
-    //console.log(currentProject);
-    refresh();
-    thumbnail.dataset.open = "1";
 
-    const startBox = thumbnail.getBoundingClientRect();
-    project.style.top = `${startBox.top}px`;
-    project.style.left = `${startBox.left}px`;
-    project.style.width = `${startBox.width}px`;
-    project.style.height = `${startBox.height}px`;
-    project.style.fontSize = "5px";
-    project.style.lineHeight = "1.2";
-    project.style.display = "flex";
-    project.hidden = false;
+    thumbnail.dataset.open = "1";
+    const project = document.querySelector(`.${landscape ? "landscape" : "portrait"} > #${thumbnail.classList[0]}`);
+    currentProject = project;
+    refresh();
+
+    const clone = thumbnail.parentElement.cloneNode();
+    clone.style.backgroundImage = getComputedStyle(thumbnail).backgroundImage;
+    clone.classList.add("thumbnail-clone");
+    clone.id = "clone";
+
+    const box = thumbnail.getBoundingClientRect();
+    clone.style.top = `${box.top}px`;
+    clone.style.left = `${box.left}px`;
+    clone.style.height = `${box.height}px`;
+    clone.style.width = `${box.width}px`;
+    document.body.appendChild(clone);
+
+    const newBackground = document.createElement("div");
+    newBackground.classList.add("thumbnail-background");
+    newBackground.style.backgroundImage = getComputedStyle(currentProject).backgroundImage;
+    clone.appendChild(newBackground);
+
+    const newTop = landscape ? `${(100 - heightPercent * 100) / 2}vh` : "2.5px";
+    const newLeft = landscape ? `${(100 - widthPercent * 100) / 2}vw` : "2.5px";
+    const newWidth = `${widthPercent * 100}vw`;
+    const newHeight = `${heightPercent * 100}vh`;
     setTimeout(() => {
-        project.style.top = `${0.5 * window.visualViewport.height - projectHeight / 2 + window.visualViewport.offsetTop}px`;
-        project.style.left = `${0.5 * window.visualViewport.width - projectWidth / 2 + window.visualViewport.offsetLeft}px`;
-        //project.style.width = `${projectWidth}px`;
-        project.style.width = "80vw";
-        //project.style.height = `${projectHeight}px`;
-        project.style.height = "90vh";
-        project.style.opacity = "1";
-        project.style.backgroundSize = "100% 100%";
-        project.style.fontSize = "16px";
-        project.style.lineHeight = "1.2";
+        clone.style.top = newTop;
+        clone.style.left = newLeft;
+        clone.style.width = newWidth;
+        clone.style.height = newHeight;
+        newBackground.style.opacity = "1";
     }, 1);
+    
+    project.style.top = newTop;
+    project.style.left = newLeft;
+    project.style.width = newWidth;
+    project.style.height = newHeight;
+    project.style.fontSize = "5px";
+    project.style.display = "flex";
+    
     document.querySelector("#back").style.zIndex = "2";
     document.querySelector("#back").style.opacity = "0.6";
+
+    setTimeout(() => {
+        project.scrollTop = 0;
+        project.scrollMarginTop = 0;
+    }, 500);
+    setTimeout(() => {
+        project.style.opacity = "1";
+        project.style.fontSize = "16px";
+    }, 750);
+
+    document.body.style.overflowY = "hidden";
 };
 
 const closeProject = function () {
     if (currentProject) {
         const project = currentProject;
         currentProject = null;
+        
         const thumbnail = document.querySelector(`.${project.id}.thumbnail`);
-        thumbnail.dataset.open = "0";
-        const transition = getComputedStyle(project).transition;
-        project.style.transition = "none";
-        project.style.top = `${project.getBoundingClientRect().top + window.scrollY}px`;
-        project.style.position = "absolute";
+        const clone = document.querySelector("#clone");
+        const transition = getComputedStyle(clone).transition;
+        clone.style.transition = "none";
+
+        let box = clone.getBoundingClientRect();
+        clone.style.position = "absolute";
+        clone.style.top = `${box.top + window.scrollY}px`;
+        clone.style.left = `${box.left}px`;
+        clone.style.height = `${box.height}px`;
+        clone.style.width = `${box.width}px`;
+
+        project.style.opacity = "0";
         setTimeout(() => {
-            project.style.transition = transition;
-            const box = thumbnail.getBoundingClientRect();
-            project.style.top = `${box.top + window.scrollY}px`;
-            project.style.left = `${box.left}px`;
-            project.style.width = `${box.width}px`;
-            project.style.height = `${box.height}px`
-            project.style.opacity = "0";
-            project.style.backgroundSize = "100% 100%";
-            project.style.fontSize = "5px";
-            project.style.lineHeight = "1.2";
-            project.scrollTop = 0;
+            clone.style.transition = transition;
+            box = thumbnail.getBoundingClientRect();
+            clone.style.top = `${box.top + window.scrollY}px`;
+            clone.style.left = `${box.left}px`;
+            clone.style.height = `${box.height}px`;
+            clone.style.width = `${box.width}px`;
+
+            project.style.display = "none";
             document.querySelector("#back").style.opacity = "0";
+            clone.querySelector("div").style.opacity = "0";
+
             setTimeout(() => {
                 document.querySelector("#back").style.zIndex = "-2";
             }, 500);
             setTimeout(() => {
-                project.style.display = "none";
-                project.style.position = "fixed";
-            }, Math.max(...getComputedStyle(project).transitionDuration.split(" ").map((d) => {
-                return Number(d.split("s")[0]);
-            })) * 1000);
-        }, 1);
+                clone.remove();
+                thumbnail.dataset.open = "0";
+            }, 750);
+        }, 500);
+
+        document.body.style.overflowY = "initial";
     }
 };
 
@@ -103,14 +174,13 @@ document.addEventListener("click", function (event) {
 const backgroundAnimation = function () {
     setInterval(() => {
         const char = document.createElement("p");
-        char.className = "randLetter";
+        char.classList.add("randLetter");
         const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-=;:\'\"[{]}\\|`~,<.>/?";
         char.innerHTML = characters.charAt(Math.floor(Math.random() * characters.length));
         const main = document.querySelector("#main").getBoundingClientRect();
         char.style.left = `${Math.random() * (main.width * 0.8) + main.width * 0.1}px`;
         char.style.top = `${Math.random() * (main.height * 0.9)}px`;
         document.querySelector("#background").appendChild(char);
-        //char.style.opacity = "0.6";
         char.style.animationPlayState = "running";
         setTimeout(() => removeChar(char), 3000);
     }, 100);
@@ -122,42 +192,9 @@ const removeChar = function (element) {
     setTimeout(() => {
         element.remove();
     }, 2000);
-    //element.style.opacity = "0";
 };
 
-document.addEventListener("scroll", (event) => {
-    const nav = document.querySelector("#hidden-nav");
-    const navHeight = document.querySelector("nav").getBoundingClientRect().top - parseFloat(getComputedStyle(nav).paddingTop) + window.scrollY;
-    //console.log("onscroll", navHeight);
-    nav.style.visibility = window.scrollY > navHeight ? "visible" : "hidden";
+document.addEventListener("scroll", () => {
+    const nav = document.querySelector("nav");
+    nav.classList.toggle("floating", landscape && nav.getBoundingClientRect().top === parseInt(getComputedStyle(nav).top));
 });
-
-window.onload = function (event) {
-    backgroundAnimation();
-};
-
-const fixAnchors = function () {
-    const nav = document.querySelector("#hidden-nav");
-    document.querySelectorAll(".anchor").forEach((element) => {
-        element.style.scrollMarginTop = `${nav.getBoundingClientRect().height + 30}px`;
-    });
-};
-
-const revealDetails = function (details) {
-    if (details.dataset.open == "1") {
-        details.style.height = `${details.querySelector(".button.details").getBoundingClientRect().height}px`;
-        details.dataset.open = "0";
-        details.querySelector(".arrow").style.transform = "rotate(0)";
-    } else {
-        details.style.height = `${details.querySelector(".button.details").getBoundingClientRect().height + details.querySelector(".details-table").getBoundingClientRect().height}px`;
-        details.dataset.open = "1";
-        details.querySelector(".arrow").style.transform = "rotate(90deg)";
-    }
-};
-
-const changePage = function (button) {
-    console.log(currentProject.scrollTop);
-    const pages = Array.from(currentProject.querySelectorAll("div.page"));
-    const page = pages.indexOf(button.closest("div.page")) + (button.dataset.dir === "down" ? 1 : -1);
-    currentProject.scroll(0, page * projectHeight);
-};
